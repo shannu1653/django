3b7ffc6d6b7a8a6758ced6233b5e3735d6a40676
shannu1653 <pentashanmukha2002@gmail.com>
2025-12-07 22:29:00 +0530

    models.py

--- myproject/basic/models.py
+++ myproject/basic/models.py
@@ -13,3 +13,20 @@ class PostData(models.Model):
     post_type = models.CharField(max_length=50)
     post_date = models.DateField()       
     post_description = models.CharField(max_length=1000)  # ✅ FIXED
+
+
+#3.class creating for signup page
+class Users(models.Model):
+    username=models.CharField(max_length=100,unique=True)
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length=100)
+    
+#4.class creating for movie details
+class Movie(models.Model):
+    Movie_name=models.CharField(max_length=100,unique=True)
+    Date=models.DateField()
+    Budget=models.CharField(max_length=100)
+    Rating = models.CharField(max_length=10)
+
+
+
